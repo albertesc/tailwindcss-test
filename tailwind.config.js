a758c9412000f3b74991d909539af503bda22f85
Albert <albert.esc@latevaweb.com>
2020-11-02 11:10:20 +0100

    add content before utility and add button styles in base.sass

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 const _ = require("lodash");
+const plugin = require('tailwindcss/plugin');
 
 const customColors = {
     primary: {
@@ -32,68 +33,6 @@ module.exports = {
             },
             backgroundColor: {
                 ...customColors
-            },
-            cursor: {
-                "col-resize": "col-resize"
-            }
-        },
-        buttons: theme => ({
-            primary: {
-                backgroundColor: theme("colors.primary.500"),
-                backgroundColorHover: theme("colors.primary.400"),
-                textColor: "#FFFFFF",
-                textColorHover: "#FFFFFF",
-                padding: "0.75rem 2.5rem",
-                borderRadius: ".25rem",
-                lineHeight: "1.25",
-                borderWidth: "1px",
-                borderStyle: "solid",
-                borderColor: theme("colors.primary.500"),
-                borderColorHover: theme("colors.primary.400"),
-            },
-            ghost: {
-                backgroundColor: "transparent",
-                backgroundColorHover: theme("colors.primary.500"),
-                textColor: theme("colors.primary.500"),
-                textColorHover: "#FFFFFF",
-                padding: "0.75rem 2.5rem",
-                borderRadius: ".25rem",
-                lineHeight: "1.25",
-                borderWidth: "1px",
-                borderStyle: "solid",
-                borderColor: theme("colors.primary.500"),
-                borderColorHover: theme("colors.primary.500"),
-            },
-            link: {
-                backgroundColor: "transparent",
-                backgroundColorHover: "transparent",
-                textColor: theme("colors.primary.500"),
-                textColorHover: theme("colors.primary.400"),
-                padding: "0",
-                borderRadius: "0",
-                lineHeight: "1.25"
-            }
-        }),
-        buttonSizes: {
-            sm: {
-                fontSize: "0.75rem",
-                padding: "0.5rem 1rem"
-            }
-        },
-        customForms: {
-            default: {
-                checkbox: {
-                    "&:focus": {
-                        boxShadow: "none",
-                        borderColor: "none"
-                    }
-                },
-                radio: {
-                    "&:focus": {
-                        boxShadow: "none",
-                        borderColor: "none"
-                    }
-                }
             }
         },
     },
@@ -103,19 +42,21 @@ module.exports = {
         width: ['responsive', 'group-active'],
         height: ['responsive', 'group-active'],
         display: ['responsive', 'group-active'],
-        backgroundColor: ['responsive', 'hover', 'focus', 'group-active', 'before'],
+        backgroundColor: ['responsive', 'hover', 'focus', 'group-active'],
         scale: ['responsive', 'hover', 'focus', 'group-active'],
         translate: ['responsive', 'hover', 'focus', 'group-active'],
     },
     plugins: [
         require("@tailwindcss/ui"),
-        require("@tailwindcss/custom-forms"),
-        require("tailwindcss-buttons")({
-            sizes: true
-        }),
         require("./plugins/classHeaderFixed")(),
         require("./plugins/classActive")(),
-        require("./plugins/variantBefore")(),
+        plugin(function({ addComponents }){
+            addComponents({
+                '.content': {
+                    'content': '""'
+                }
+            });
+        })
     ],
     purge: [
         './public/**/*.html',
